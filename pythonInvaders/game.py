16c11d6a7cfc3635c2f382d751c425e1373deb50
Felix Bärring <felix_barring@hotmail.com>
2017-05-22 20:45:02 +0200

    Fixed so that the movement depend on when a key is down, not if it has been pressed.

--- pythonInvaders/game.py
+++ pythonInvaders/game.py
@@ -14,25 +14,53 @@ screenHeight = 600
 class Player:
   screen = None
   playerSprite = None
+  projectiles = None
   
   xPos = 400.0
   yPos = 550.0
 
   movementSpeed = 0.1
 
-  def __init__(self, s):
-    print "### Player \n"
-    self.screen = s
+  def __init__(self, screen, projectiles):
+    self.screen = screen
+    self.projectiles = projectiles
     self.playerSprite = pygame.image.load('data/player.png').convert()
 
-  def update(self, moveLeft, moveRight):
-    if moveLeft: self.xPos += self.movementSpeed
-    if moveRight: self.xPos -= self.movementSpeed
+  def update(self, keys):
+    if keys[pygame.K_RIGHT]: 
+      self.xPos += self.movementSpeed
+
+    if keys[pygame.K_LEFT]: 
+      self.xPos -= self.movementSpeed
+    
+    if keys[pygame.K_SPACE]:
+#      projectile = Projectile(self.screen, self.xPos, self.yPos, -1)
+      self.projectiles.append(projectile)
+
     self.screen.blit(self.playerSprite, (self.xPos, self.yPos))
+    
+class Projectile:
+  screen = None
+  sprite = None
+
+  xPos = None
+  yPos = None
+
+  movementSpeed = 0.3
+  direction = None
+
+  def __init__(self, screen, x, y, direction):
+    self.screen = screen
+    self.sprite = pygame.image.load('data/projectile.png').convert()
+    self.xPos = x
+    self.yPos = y
+    self.direction = direction
+
+  def update(self):
+    self.yPos += self.movementSpeed * self.direction
+    self.screen.blit(self.sprite, (self.xPos, self.yPos))
 
 def main():
-
-  print "### Main \n"
 
   pygame.init()
   screen = pygame.display.set_mode((screenWidth, screenHeight))
@@ -45,18 +73,23 @@ def main():
   screen.blit(background, (0,0))
   pygame.display.flip()
 
-  player = Player(screen)
+  projectiles = []
+  player = Player(screen, projectiles)
 
   while True:
     for event in pygame.event.get():
       if (event.type == pygame.QUIT or event.type == pygame.KEYDOWN 
           and event.key == pygame.K_ESCAPE) : 
         sys.exit()
-      
-      moveRight = event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT
-      moveLeft = event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT
 
-    player.update(moveRight, moveLeft)
+    keys = pygame.key.get_pressed()
+
+    player.update(keys)
+
+    for projectile in projectiles:
+      projectile.update()
+
     pygame.display.flip()
                 
 if __name__ == '__main__': main()
+
